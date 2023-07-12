6042bc7c6c686e399a6b409e82b1f3d1d69e80f3
hemz10 <sph2740@example.com>
2023-07-12 18:15:19 +0530

    feat: get running enclaves

--- cli/commands/clean/clean.go
+++ cli/commands/clean/clean.go
@@ -4,6 +4,10 @@ Copyright © 2023 Hugobyte AI Labs<hello@hugobyte.com>
 package clean
 
 import (
+	"context"
+
+	"github.com/kurtosis-tech/kurtosis/api/golang/engine/lib/kurtosis_context"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
@@ -13,5 +17,32 @@ var CleanCmd = &cobra.Command{
 	Short: "Cleans up Kurtosis leftover artifacts",
 	Long:  `Destroys and removes any running encalves. If no enclaves running to remove it will throw an error`,
 	Run: func(cmd *cobra.Command, args []string) {
+		ctx := context.Background()
+		kurtosisCtx, err := kurtosis_context.NewKurtosisContextFromLocalEngine()
+		logrus.Info("Trying to connect to local Kurtosis Engine...")
+		if err != nil {
+			logrus.Errorf("Connecting to kurtosis engine failed as kurtosis engine is not running")
+		}
+		enclaveName := getEnclaves(ctx, kurtosisCtx)
+		if enclaveName == "" {
+			logrus.Errorf("No enclaves running to clean !!")
+		} else {
+			// clean(ctx, kurtosisCtx)
+		}
 	},
+}
+
+// To get names of running enclaves, returns empty string if no enclaves
+func getEnclaves(ctx context.Context, kurtosisCtx *kurtosis_context.KurtosisContext) string {
+	enclaves, err := kurtosisCtx.GetEnclaves(ctx)
+	if err != nil {
+		logrus.Errorf("Getting Enclaves failed with error:  %v", err)
+	}
+	enclaveMap := enclaves.GetEnclavesByName()
+	for _, enclaveInfoList := range enclaveMap {
+		for _, enclaveInfo := range enclaveInfoList {
+			return enclaveInfo.GetName()
+		}
+	}
+	return ""
 }
