0a0e258a764c2f0209f6073aea1ab3c37d282999
hemz10 <sph2740@example.com>
2023-07-12 14:30:14 +0530

    feat: Add version constant

---
+++ cli/commands/version/dive_version.go
@@ -0,0 +1,7 @@
+package version
+
+const (
+	// !!!!!!!!!!! DO NOT UPDATE! WILL BE MANUALLY UPDATED DURING THE RELEASE PROCESS !!!!!!!!!!!!!!!!!!!!!!
+	DiveVersion = "v 0.0.1(alpha)"
+	// !!!!!!!!!!! DO NOT UPDATE! WILL BE MANUALLY UPDATED DURING THE RELEASE PROCESS !!!!!!!!!!!!!!!!!!!!!!
+)
