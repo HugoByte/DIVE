47c33e86486d5ecab798adfd522eea858bf744e9
hemz10 <sph2740@example.com>
2023-07-12 18:05:19 +0530

    feat: Implementation for dive clean command

--- cli/commands/clean/clean.go
+++ cli/commands/clean/clean.go
@@ -4,22 +4,60 @@ Copyright © 2023 Hugobyte AI Labs<hello@hugobyte.com>
 package clean
 
 import (
-	"fmt"
+	"context"
 
+	"github.com/kurtosis-tech/kurtosis/api/golang/engine/lib/kurtosis_context"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
 // cleanCmd represents the clean command
 var CleanCmd = &cobra.Command{
 	Use:   "clean",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Cleans up Kurtosis leftover artifacts",
+	Long:  `Destroys and removes any running encalves. If no enclaves running to remove it will throw an error`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("clean called")
+		ctx := context.Background()
+		kurtosisCtx, err := kurtosis_context.NewKurtosisContextFromLocalEngine()
+		logrus.Info("Trying to connect to local Kurtosis Engine...")
+		if err != nil {
+			logrus.Errorf("Connecting to kurtosis engine failed as kurtosis engine is not running")
+		}
+		enclaveName := getEnclaves(ctx, kurtosisCtx)
+		if enclaveName == "" {
+			logrus.Errorf("No enclaves running to clean !!")
+		} else {
+			clean(ctx, kurtosisCtx)
+		}
 	},
+}
+
+// To get names of running enclaves, returns empty string if no enclaves
+func getEnclaves(ctx context.Context, kurtosisCtx *kurtosis_context.KurtosisContext) string {
+	enclaves, err := kurtosisCtx.GetEnclaves(ctx)
+	if err != nil {
+		logrus.Errorf("Getting Enclaves failed with error:  %v", err)
+	}
+	enclaveMap := enclaves.GetEnclavesByName()
+	for _, enclaveInfoList := range enclaveMap {
+		for _, enclaveInfo := range enclaveInfoList {
+			return enclaveInfo.GetName()
+		}
+	}
+	return ""
+}
+
+// Funstionality to clean the enclaves
+func clean(ctx context.Context, kurtosisCtx *kurtosis_context.KurtosisContext) {
+	logrus.Info("Successfully connected to kurtosis engine...")
+	logrus.Info("Initializing cleaning process...")
+
+	// shouldCleanAll set to true as default for beta release.
+	enclaves, err := kurtosisCtx.Clean(ctx, true)
+	if err != nil {
+		logrus.Errorf("Failed cleaning with error: %v", err)
+	}
+
+	// Assuming only one enclave is running for beta release
+	logrus.Infof("Successfully destroyed and cleaned enclave %s", enclaves[0].Name)
 }
