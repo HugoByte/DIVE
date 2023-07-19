1259c20c25673360ebfaf2128129e91150574df3
Shreyas S Bhat <35568964+shreyasbhat0@users.noreply.github.com>
2023-07-19 19:22:38 +0530

    fix: remove dive.json on clean command (#52)

--- cli/commands/clean/clean.go
+++ cli/commands/clean/clean.go
@@ -4,6 +4,8 @@ Copyright © 2023 Hugobyte AI Labs<hello@hugobyte.com>
 package clean
 
 import (
+	"os"
+
 	"github.com/hugobyte/dive/common"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
@@ -17,6 +19,18 @@ func NewCleanCmd(diveContext *common.DiveContext) *cobra.Command {
 		Long:  `Destroys and removes any running encalves. If no enclaves running to remove it will throw an error`,
 		Run: func(cmd *cobra.Command, args []string) {
 			common.ValidateCmdArgs(args, cmd.UsageString())
+			pwd, err := os.Getwd()
+
+			if err != nil {
+				diveContext.FatalError("Failed cleaning with error: %v", err.Error())
+			}
+
+			_, err = os.Stat(pwd + "/dive.json")
+
+			if err == nil {
+				os.Remove(pwd + "/dive.json")
+			}
+
 			enclaveName := diveContext.GetEnclaves()
 			if enclaveName == "" {
 				logrus.Errorf("No enclaves running to clean !!")
