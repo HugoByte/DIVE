0625936c7c266c5a538cea34d7a540cf2605d805
shreyasbhat0 <shreyas.ks@hugobyte.com>
2023-07-27 17:42:21 +0530

    chore: update dive version

--- cli/common/constants.go
+++ cli/common/constants.go
@@ -3,7 +3,7 @@ package common
 var DiveLogs bool
 
 // !!!!!!!!!!! DO NOT UPDATE! WILL BE UPDATED DURING THE RELEASE PROCESS !!!!!!!!!!!!!!!!!!!!!!
-var DiveVersion = "v0.0.1-beta"
+var DiveVersion = "v0.0.2-beta"
 
 const (
 	DiveEnclave                  = "dive"
