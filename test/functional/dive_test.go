03d7aef6d5c0d78fbaab8dcc5af8fac41fd0bb70
hemz10 <sph2740@example.com>
2023-08-14 13:03:37 +0530

    test: add smoke test cases

--- test/functional/dive_test.go
+++ test/functional/dive_test.go
@@ -1,8 +1,14 @@
 package dive_test
 
 import (
+	"bytes"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
 	"testing"
 
+	"github.com/hugobyte/dive/common"
 	"github.com/onsi/ginkgo"
 	"github.com/onsi/gomega"
 )
@@ -11,3 +17,96 @@ func TestCLIApp(t *testing.T) {
 	gomega.RegisterFailHandler(ginkgo.Fail)
 	ginkgo.RunSpecs(t, "CLI App Suite")
 }
+
+type testWriter struct {
+	buffer bytes.Buffer
+}
+
+func (w *testWriter) Write(p []byte) (n int, err error) {
+	w.buffer.Write(p)
+	os.Stdout.Write(p)
+	return len(p), nil
+}
+
+func GetBinaryPath() string {
+	workingDir, _ := os.Getwd()
+	binaryPath := filepath.Join(workingDir, "/../../cli/dive")
+	return binaryPath
+}
+
+func Clean() {
+	var cmd *exec.Cmd
+	binaryPath := GetBinaryPath()
+	cmd = exec.Command(binaryPath)
+	cmd.Args = append(cmd.Args, "clean")
+	fmt.Println(cmd)
+	var stdout bytes.Buffer
+	cmd.Stdout = &stdout
+	err := cmd.Run()
+	fmt.Println(stdout.String())
+	gomega.Expect(err).NotTo(gomega.HaveOccurred())
+}
+
+var _ = ginkgo.Describe("DIVE CLI App", func() {
+	var cmd *exec.Cmd
+	binaryPath := GetBinaryPath()
+
+	ginkgo.BeforeEach(func() {
+		cmd = exec.Command(binaryPath)
+		cmd.Stdout = &testWriter{}
+		cmd.Stderr = &testWriter{}
+	})
+
+	ginkgo.Describe("Smoke Tests", func() {
+		ginkgo.It("should display the correct version", func() {
+			cmd.Args = append(cmd.Args, "version")
+			var stdout bytes.Buffer
+			cmd.Stdout = &stdout
+
+			err := cmd.Run()
+			fmt.Println(stdout.String())
+			gomega.Expect(err).NotTo(gomega.HaveOccurred())
+			latestVersion := common.GetLatestVersion()
+			gomega.Expect(stdout.String()).To(gomega.ContainSubstring(latestVersion))
+		})
+
+		ginkgo.It("should open twitter page on browser", func() {
+			cmd.Args = append(cmd.Args, "twitter")
+			err := cmd.Run()
+			gomega.Expect(err).NotTo(gomega.HaveOccurred())
+		})
+
+		ginkgo.It("should open Youtube Tutorial Channel on browser", func() {
+			cmd.Args = append(cmd.Args, "tutorial")
+			err := cmd.Run()
+			gomega.Expect(err).NotTo(gomega.HaveOccurred())
+		})
+
+		ginkgo.It("should open Discord channel on browser", func() {
+			cmd.Args = append(cmd.Args, "discord")
+			err := cmd.Run()
+			gomega.Expect(err).NotTo(gomega.HaveOccurred())
+		})
+
+		ginkgo.It("should start bridge between icon and eth", func() {
+			cmd.Args = append(cmd.Args, "bridge", "btp", "--chainA", "icon", "--chainB", "eth")
+			err := cmd.Run()
+			gomega.Expect(err).NotTo(gomega.HaveOccurred())
+		})
+
+		ginkgo.It("should start bridge icon and hardhat but with icon bridge set to true", func() {
+			Clean()
+			cmd.Args = append(cmd.Args, "bridge", "btp", "--chainA", "icon", "--chainB", "hardhat", "--bmvbridge")
+			err := cmd.Run()
+			gomega.Expect(err).NotTo(gomega.HaveOccurred())
+		})
+
+		ginkgo.It("should start bridge icon and icon", func() {
+			Clean()
+			cmd.Args = append(cmd.Args, "bridge", "btp", "--chainA", "icon", "--chainB", "icon")
+			err := cmd.Run()
+			gomega.Expect(err).NotTo(gomega.HaveOccurred())
+		})
+
+	})
+})
