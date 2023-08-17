8fd3d561b12680f730cc95069c150abaa4627a70
Shreyas S Bhat <35568964+shreyasbhat0@users.noreply.github.com>
2023-08-17 18:18:57 +0530

    chore: bump cli version

--- cli/common/constants.go
+++ cli/common/constants.go
@@ -3,7 +3,7 @@ package common
 var DiveLogs bool
 
 // !!!!!!!!!!! DO NOT UPDATE! WILL BE UPDATED DURING THE RELEASE PROCESS !!!!!!!!!!!!!!!!!!!!!!
-var DiveVersion = "v0.0.6-beta"
+var DiveVersion = "v0.0.7-beta"
 
 const (
 	DiveEnclave                  = "dive"
