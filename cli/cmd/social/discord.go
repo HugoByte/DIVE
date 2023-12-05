44ee2382d24bf3a9b951c09ed716086f3492b73a
shreyasbhat0 <shreyas.ks@hugobyte.com>
2023-12-05 16:28:50 +0530

    feat: update social  discord and twitter commands

--- cli/cmd/social/discord.go
+++ cli/cmd/social/discord.go
@@ -1,6 +1,8 @@
 package social
 
 import (
+	"fmt"
+
 	"github.com/hugobyte/dive-core/cli/common"
 	"github.com/spf13/cobra"
 )
@@ -13,4 +15,20 @@ var DiscordCmd = common.NewDiveCommandBuilder().
 	SetLong(`The command opens the Discord channel for DIVE, providing a direct link or launching the Discord application to access the dedicated DIVE community. It allows users to engage in discussions, seek support, share insights, and collaborate with other members of the DIVE community within the Discord platform.`).
 	SetRun(discord).Build()
 
-func discord(cmd *cobra.Command, args []string) {}
+func discord(cmd *cobra.Command, args []string) {
+
+	cli := common.GetCli()
+
+	cli.Logger().SetOutputToStdout()
+	err := common.ValidateArgs(args)
+
+	if err != nil {
+		cli.Logger().Error(common.CodeOf(err), common.Errorc(common.CodeOf(err), fmt.Sprintf("error %s \n %s ", err, cmd.UsageString())).Error())
+	}
+
+	cli.Logger().Info("Redirecting to DIVE discord channel...")
+
+	if err := common.OpenFile(diveURL); err != nil {
+		cli.Logger().Fatalf(common.CodeOf(err), "Failed to open Dive discord channel with error %v", err)
+	}
+}
