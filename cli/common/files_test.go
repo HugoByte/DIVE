82773379ccad0a336249eaa40ec46d5a8bd7025a
shreyasbhat0 <shreyas.ks@hugobyte.com>
2024-01-17 15:47:51 +0530

    test: fix unit test

--- cli/common/files_test.go
+++ cli/common/files_test.go
@@ -2,7 +2,9 @@ package common
 
 import (
 	"encoding/json"
+	"fmt"
 	"os"
+	"path/filepath"
 	"testing"
 )
 
@@ -39,11 +41,22 @@ func TestDiveFileHandler_ReadFile(t *testing.T) {
 func TestDiveFileHandler_ReadJson(t *testing.T) {
 	// Create a temporary file for testing
 
-	tempFile, err := os.Create("testfile.json")
+	pwd, err := os.Getwd()
 	if err != nil {
 		t.Fatal(err)
 	}
-	defer os.Remove(tempFile.Name())
+
+	dirPath := filepath.Join(pwd, "output")
+	err = os.MkdirAll(dirPath, 0777)
+	if err != nil {
+		t.Fatal(err)
+	}
+	filePath := filepath.Join(dirPath, "testfile.json")
+	tempFile, err := os.Create(filePath)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(fmt.Sprintf("%s/%s", pwd, "output"))
 
 	// Write JSON data to the temporary file
 	testData := map[string]string{"key": "value"}
@@ -71,6 +84,7 @@ func TestDiveFileHandler_ReadJson(t *testing.T) {
 	if err != nil {
 		t.Fatalf("Error reading JSON file: %v", err)
 	}
+	fmt.Println(decodedData)
 
 	// Compare decoded data with the original data
 	if decodedData["key"] != testData["key"] {
